e78fc8e6947887611ab40d588c49c7e3a9f0bb33
ckurt <ckurt@student.42lyon.fr>
2021-04-06 16:36:18 +0200

    wip, genre doucement frere

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 minikube start
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+Kubectl apply -f ./srcs/metallb/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 minikube dashboard
