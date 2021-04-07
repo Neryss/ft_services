aaa032e685c9770e93879bc822c13519147c2cbc
ckurt <ckurt@student.42lyon.fr>
2021-04-07 13:39:49 +0200

    nginx kinda working

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 Kubectl apply -f ./srcs/metallb/metallb.yaml
 
+eval $(minikube docker-env)
 docker build ./srcs/nginx --rm -t my-nginx
+minikube image load my-nginx
 Kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 minikube dashboard
