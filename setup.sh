46397a5064ba04ba06ae29e6102efa4d7360b445
ckurt <ckurt@student.42lyon.fr>
2021-04-07 11:12:04 +0200

    started working on deployments basics

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 Kubectl apply -f ./srcs/metallb/metallb.yaml
+
+docker build ./srcs/nginx --rm -t my-nginx
+Kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 minikube dashboard
