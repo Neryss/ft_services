4bf2d4698c4f81275ce910ebe6020f90fee52098
Ckurt <kuroysiris@gmail.com>
2021-04-08 11:28:19 +0200

    cleaner setup, wip nginx

--- setup.sh
+++ setup.sh
@@ -1,13 +1,31 @@
-minikube start
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f - -n kube-system
-Kubectl apply -f ./srcs/metallb/metallb.yaml
-Kubectl apply -f ./srcs/metallb/namespace.yaml
+run_metallb()
+{
+	Kubectl apply -f ./srcs/metallb/namespace.yaml
+	Kubectl apply -f ./srcs/metallb/metallb.yaml
+	Kubectl apply -f ./srcs/metallb/config.yaml
+}
 
-eval $(minikube docker-env)
-docker build ./srcs/nginx --rm -t my-nginx
-minikube image load my-nginx
-Kubectl apply -f ./srcs/nginx/nginx.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+run_nginx()
+{
+	eval $(minikube docker-env)
+	docker build ./srcs/nginx --rm -t my-nginx
+	Kubectl apply -f ./srcs/nginx/nginx.yaml
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+}
+
+run_minikube()
+{
+	minikube start
+	kubectl get configmap kube-proxy -n kube-system -o yaml | \
+	sed -e "s/strictARP: false/strictARP: true/" | \
+	kubectl apply -f - -n kube-system
+}
+
+echo "Starting services"
+run_minikube
+echo "Setting up metallb..."
+run_metallb
+echo "Setting up nginx..."
+run_nginx
+echo "All done !\n Starting dashboard"
 minikube dashboard
