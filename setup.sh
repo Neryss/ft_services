13f18cc2e43fb7ac696227903f25f5a2cd592a85
Ckurt <kuroysiris@gmail.com>
2021-04-13 16:14:28 +0200

    cleaner setup, working php, wp

--- setup.sh
+++ setup.sh
@@ -1,56 +1,38 @@
-run_metallb()
-{
-	Kubectl apply -f ./srcs/metallb/namespace.yaml
-	Kubectl apply -f ./srcs/metallb/metallb.yaml
-	Kubectl apply -f ./srcs/metallb/config.yaml
-	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-}
-
-run_nginx()
-{
-	docker build ./srcs/nginx/ --rm -t my-nginx
-	Kubectl apply -f ./srcs/nginx/srcs/nginx.yaml
-}
-
-run_wordpress()
-{
-	docker build ./srcs/wordpress --rm -t my-wordpress
-	Kubectl apply -f ./srcs/wordpress/srcs/wordpress.yaml
-}
-
-run_mariadb()
-{
-	docker build ./srcs/mysql --rm -t my-mysql
-	kubectl apply -f ./srcs/mysql/srcs/mysql.yaml
-}
-
-run_phpmyadmin()
-{
-	docker build ./srcs/phpmyadmin --rm -t my-phpmyadmin
-	kubectl apply -f ./srcs/phpmyadmin/srcs/phpmyadmin.yaml
-}
-
 run_minikube()
 {
 	minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-65535
 	minikube addons enable metrics-server
 }
 
+build_all()
+{
+	docker build ./srcs/phpmyadmin --rm -t my-phpmyadmin
+	docker build ./srcs/mysql --rm -t my-mysql
+	docker build ./srcs/wordpress --rm -t my-wordpress
+	docker build ./srcs/nginx/ --rm -t my-nginx
+}
+
+deploy_all()
+{
+	kubectl apply -f ./srcs/phpmyadmin/srcs/phpmyadmin.yaml
+	kubectl apply -f ./srcs/mysql/srcs/mysql.yaml
+	kubectl apply -f ./srcs/wordpress/srcs/wordpress.yaml
+	kubectl apply -f ./srcs/nginx/srcs/nginx.yaml
+	Kubectl apply -f ./srcs/metallb/namespace.yaml
+	Kubectl apply -f ./srcs/metallb/metallb.yaml
+	Kubectl apply -f ./srcs/metallb/config.yaml
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+}
+
 echo "Starting services"
 run_minikube
 eval $(minikube docker-env)
 minikube ssh "docker login -u gapoulai -p motdepassesupersafe"
-echo "Setting up metallb..."
-run_metallb
-echo "Setting up nginx..."
-run_nginx
-echo "Setting up mariadb..."
-run_mariadb
-echo "Setting up phpmyadmin..."
-run_phpmyadmin
-echo "Setting up wordpress..."
-run_wordpress
-echo "Minikube ip is the following :\n"
+build_all
+echo "Done building every image"
+deploy_all
+echo "Done deploying..."
+echo "Minikube ip is the following :"
 minikube ip
 echo "All done !\n Starting dashboard"
 minikube dashboard
