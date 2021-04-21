e6b16996acac8d06488be719bf432508c6e46253
Ckurt <kuroysiris@gmail.com>
2021-04-21 12:37:53 +0200

    need patch ssl

--- srcs/certif-handler.sh
+++ srcs/certif-handler.sh
@@ -3,7 +3,7 @@ cd $DIR
 mkdir -p certs
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
 	-subj "/C=FR/ST=Auvergne-Rhône-Alpes/L=Lyon/O=42/CN=ckurt" \
-	-keyout certs/tls.key -out certs/tls.cert
+	-keyout certs/tls.key -out certs/tls.crt
 kubectl create secret tls ft-services-tls \
 	--cert ./certs/tls.crt \
 	--key ./certs/tls.key
