ff7d8f31eff96c5796386e1a4df6308856f90766
ckurt <ckurt@student.42lyon.fr>
2021-04-23 11:21:00 +0200

    patch nginx restart

--- srcs/mysql/srcs/config.sh
+++ srcs/mysql/srcs/config.sh
@@ -1,12 +1,21 @@
-/etc/init.d/mariadb setup
-rc-service mariadb start
+#!/bin/sh
+check_service() {
+	for var in "$@"; do
+		if [ $(/usr/bin/pgrep $var | wc -l) == 0 ]; then
+			exit 1
+		fi
+	done
+}
 
-mysql -u root -e "CREATE USER '${MYSQL_ROOT_USERNAME}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_ROOT_PASSWORD}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "CREATE DATABASE wordpress;"
+/etc/init.d/mariadb setup
+service mariadb start
+
+mysql -u root -e "create user '${MYSQL_ROOT_USERNAME}'@'%' identified by '${MYSQL_ROOT_PASSWORD}'"
+mysql -u root -e "grant all privileges on *.* to '${MYSQL_ROOT_USERNAME}'@'%'"
+mysql -u root -e "flush privileges"
+mysql -u root -e "create database wordpress"
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-rc-service mariadb restart
+service mariadb restart
 
 if [ `mysql -u root -e "use wordpress; show tables\G" | wc -l` = 0 ]
 then
@@ -15,6 +24,7 @@ fi
 
 sleep 5
 
-while [ $(/usr/bin/pgrep mysql | wc -l) -gt 0 ]; do
-		sleep 2
+while true; do
+	check_service mysql
+	sleep 2
 done
